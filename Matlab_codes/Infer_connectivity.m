33e045c22167000b6beedf058c83f6ab90026bb7
bsinghal175 <125697919+bsinghal175@users.noreply.github.com>
2024-01-17 11:52:55 -0600

    Add files via upload

---
+++ Matlab_codes/Infer_connectivity.m
@@ -0,0 +1,83 @@
+%% This code inferes the network connectivity structure using the SINC 
+%  approach. 
+
+% Inputs: 
+%       1. Model_type - 1 for Kuramoto oscillators; 2 for gene-regulatory
+%       networks & 3 for Rossler oscillators 
+
+
+
+Model_type = 3;  %change this to infer other network models 
+switch(Model_type)
+    
+     
+    %---------------------------------------------------------------------
+    case 1  %Infer the connectivity of Kuramoto oscillators
+        
+        %Algorithm hyperparameters
+        Gamma = 2;
+        Epsilon = 10^-3;
+        r = 2;
+          
+        X = readmatrix('..\Data\Kuramoto\Time_series.csv');
+        X_dot = readmatrix('..\Data\Kuramoto\Time_series_derivative.csv');
+        Adjacency_matrix =  readmatrix('..\Data\Kuramoto\Adjacency_matrix.csv');
+        Adjacency_matrix_binary = Adjacency_matrix;
+        Adjacency_matrix_binary(Adjacency_matrix_binary>0) = 1;
+
+        %Infer networks for 6 different data lengths 
+        for i = 1:6
+            results(i) = SINC_kuramoto(X(1:50*i,:),...
+                X_dot(1:50*i,:),r,Epsilon,Gamma);
+            [~,~,~,AUC(i)] = perfcurve...
+                (Adjacency_matrix_binary(:),results(i).K_est(:),1);      
+        end
+      
+    
+        
+    %---------------------------------------------------------------------
+    case 2  %Infer the connectivity of Gene Regulatory networks  
+        %Algorithm hyperparameters
+        Gamma = 2;
+        Epsilon = 10^-3;
+        r = 3;
+        
+        X = readmatrix('..\Data\Gene_regulatory\Time_series.csv');
+        X_dot = readmatrix('..\Data\Gene_regulatory\Time_series_derivative.csv');
+        Adjacency_matrix =  readmatrix('..\Data\Gene_regulatory\Adjacency_matrix.csv');
+        Adjacency_matrix_binary = Adjacency_matrix;
+        Adjacency_matrix_binary(Adjacency_matrix_binary>0) = 1;
+        
+        %Infer networks for 8 different data lengths 
+        count = 1;
+        for i = 1:2:15
+            results(count) = SINC_GRN(X(1:40*i,:),...
+                X_dot(1:40*i,:),r,Epsilon,Gamma);
+            [~,~,~,AUC(count)] = perfcurve...
+                (Adjacency_matrix_binary(:),results(count).K_est(:),1);  
+            count = count+1;
+        end
+     
+        
+    %---------------------------------------------------------------------
+    case 3  %Infer the connectivity of Rossler oscillator networks
+        %Algorithm hyperparameters
+        Gamma = 2;
+        Epsilon = 10^-3;
+        r = 3;
+        
+        X = readmatrix('..\Data\Rossler\Time_series.csv');
+        X_dot = readmatrix('..\Data\Rossler\Time_series_derivative.csv');
+        Adjacency_matrix =  readmatrix('..\Data\Rossler\Adjacency_matrix.csv');
+        Adjacency_matrix_binary = Adjacency_matrix;
+        Adjacency_matrix_binary(Adjacency_matrix_binary>0) = 1;
+        
+        %Infer networks for 6 different data lengths 
+        for i = 1:6
+            results(i) = SINC_rossler(X(1:50*i,:),...
+                X_dot(1:50*i,:),r,Epsilon,Gamma);
+            [~,~,~,AUC(i)] = perfcurve...
+                (Adjacency_matrix_binary(:),results(i).K_est(:),1);      
+        end
+        
+end
---
+++ Matlab_codes/SINC_rossler.m
@@ -0,0 +1,138 @@
+function results = SINC_rossler(x,x_dot,r,epsilon,Gamma)
+    
+    % x : time-series data of size MxN. 
+    % x_dot : Derivative of the data
+    % r : Number of Fourier terms used for approximation
+    % epsilon: Convergence criteria 
+    % Gamma : number of network iterations
+    x_dot = x_dot(:,1:3:end);
+    N = size(x_dot,2);
+    K_est = zeros(N,N);
+    
+    
+    
+    %Initialize and normalize the Fourier Coefficients
+    Lambda_initial = ones(r^2,1);
+    Lambda_initial = (1/norm(Lambda_initial) )*Lambda_initial;
+    
+    Iteration_number = zeros(N,Gamma);
+    Lambda_hat = Lambda_initial;
+    Lambda_each_node = zeros(N*Gamma,r^2);
+    %B_each_node = zeros(N*Gamma,N-1);
+    %A_each_node = zeros(N*Gamma,r);
+    for network_iteration = 1:Gamma
+        % i denotes the node number  
+        for i = 1:N
+          
+            F = Estimate_F_matrix(x(:,3*(i-1)+1:3*i),r);
+            Z = Estimate_Z_matrix(x(:,1:3:end),r,i);
+     
+            
+            
+            % Estimate the connection strength(B) and the self-dynamics(A)
+            for iter = 1:100
+                [B_hat,A_hat] = Estimate_topology(x_dot(:,i),F,Z,Lambda_hat,N);
+                Lambda_hat_updated = Estimate_lambda(x_dot(:,i),F,Z,B_hat,A_hat,N,r);
+                
+                B_hat = B_hat*norm(Lambda_hat_updated);
+                Lambda_hat_updated = Lambda_hat_updated/norm(Lambda_hat_updated);
+                
+                if(norm(Lambda_hat_updated-Lambda_hat)<epsilon || iter == 100)
+                    Lambda_hat = Lambda_hat_updated;
+                    Iteration_number(i,network_iteration) = iter;
+      
+                    
+                    % Store the values
+                    Lambda_each_node((network_iteration-1)*N+i,:) = Lambda_hat';
+                    %B_each_node((network_iteration-1)*N+i,:) = B_hat';
+                    %A_each_node((network_iteration-1)*N+i,:) = A_hat';
+                    break;
+                end
+                Lambda_hat = Lambda_hat_updated;
+            end
+        end
+    end
+    
+    Lambda = mean(Lambda_each_node((Gamma-1)*N:Gamma*N,:));
+    for i = 1:N
+        F = Estimate_F_matrix(x(:,3*(i-1)+1:3*i),r);
+        Z = Estimate_Z_matrix(x(:,1:3:end),r,i);
+        [B_hat(:,i),A_hat(:,i)] = Estimate_topology(x_dot(:,i),F,Z,Lambda',N);
+    end
+             
+    % Estimate the coupling matrix
+    for i = 1:N
+        for j= 1:N
+            if(i<j)
+                K_est(i,j) = B_hat(j-1,i);
+            elseif(i>j)
+                K_est(i,j) = B_hat(j,i);
+            end
+        end
+    end
+           
+    results.K_est = K_est;
+    results.A_est = A_hat;
+    results.Lambda = Lambda;
+    results.iteration = Iteration_number;
+    results.Lambda_iteration = Lambda_each_node;
+
+
+end
+
+
+%% Functions used in the main file
+function Z = Estimate_Z_matrix(x,r,node_index)
+    M = size(x,1);
+    N = size(x,2);
+    
+    Z = zeros(M*(N-1),r^2);
+    
+    x_temp = x;
+    x_temp(:,node_index) = [];
+    for t = 1:M
+        Z_i = zeros(N-1,r^2);
+        for j=0:r-1
+            for k = 1:r
+                Z_i(:,(j)*r+k) = (x(t,node_index)^j).*(x_temp(t,:)'.^k);
+            end
+        end
+        Z((t-1)*(N-1)+1:t*(N-1),:) = Z_i;
+    end
+    
+end
+
+function F = Estimate_F_matrix(x,r)
+    
+    M = size(x,1);
+    F = zeros(M,3*r);
+    for j = 0:r-1
+        F(:,3*j+1:3*(j+1)) = x.^j;
+    end 
+end
+
+
+function [B_hat,A_hat] = Estimate_topology(x_dot,F,Z,Lambda,N)
+    M = size(x_dot,1);
+    X = zeros(M,N-1);
+   
+    for i = 1:M
+        X(i,:) = (Z((i-1)*(N-1)+1:i*(N-1),:)*Lambda)';
+    end
+    
+    Temp = eye(M)-(F*pinv(F));
+    
+    B_hat = lsqnonneg(Temp*X,Temp*x_dot);
+    A_hat = pinv(F)*(x_dot-X*B_hat);
+end
+
+function Lambda_hat = Estimate_lambda(x_dot,F,Z,B_hat,A_hat,N,r)
+    M = size(x_dot,1);
+    X = zeros(M,r^2);
+    
+    for i = 1:M
+        X(i,:) = B_hat'*Z((i-1)*(N-1)+1:i*(N-1),:);
+    end
+    
+    Lambda_hat = pinv(X)*(x_dot-F*A_hat);
+end
